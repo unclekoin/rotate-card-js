7f1c0b72fac15fdda991496a02501bdc86a43c1e
Pavel Koryakin <unclekoin@gmail.com>
2020-12-06 00:07:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper');
+const cards = document.querySelectorAll('.card__inner');
+const audio = document.querySelector('.audio');
+const sound = document.querySelector('.sound');
+
+cards.forEach((card, index) => {
+  card.addEventListener('click', function () {
+    card.classList.add('is-flipped');
+  });
+  card.addEventListener('mouseleave', function () {
+    card.classList.remove('is-flipped');
+  });
+});
+
+// Вот в этом месте чего-то не могу разобраться
+function playAudio() {
+  audio.play();
+}
+
+wrapper.addEventListener('click', (e) => {
+  const cardInner = e.target.closest('.card__inner');
+  sound.src = `audio/${cardInner.getAttribute('number')}.mp3`
+  cardInner.addEventListener('click', playAudio);
+})
+
